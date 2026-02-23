93d324a2a7fecdb3774556af4aff3f05de0f08d4
Aoran_Lee <liaoran327@163.com>
2026-02-23 16:12:16 +0800

    2.3.4训练默认选择特征融合

--- train.py
+++ train.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
     parser.add_argument('--wd', default=-5, type=float, help='weight decay pow')
 
     parser.add_argument('-i', '--display_iter', default=5, type=int)#控制多少次迭代打印一次训练中间信息
-    parser.add_argument('-n', '--network', default='unet_shared', type=str)#主分割模型名称
+    parser.add_argument('-n', '--network', default='unet_multitask', type=str)#主分割模型名称
     #Gating 网络名称
     parser.add_argument('-gn', '--gating_network', default='multi_gating_attention', type=str)
     parser.add_argument('--exp_name', default='exp_shared_encoder_baseline', type=str)
