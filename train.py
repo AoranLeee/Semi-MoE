a15d024bdc64e81287c597944ec8b77a9e606d94
Aoran_Lee <liaoran327@163.com>
2026-03-23 20:24:59 +0800

    3.1.2增加alpha控制U值

--- train.py
+++ train.py
@@ -496,10 +496,14 @@ if __name__ == '__main__':
             U_sdf = U_sdf.detach()
             U_bnd = U_bnd.detach()
 
+            alpha_seg = 0.2
+            alpha_sdf = 0.1
+            alpha_bnd = 0.3
+
             # uncertainty weighting (pixel-wise)
-            W_seg = 1.0 / (1.0 + U_seg)
-            W_sdf = 1.0 / (1.0 + U_sdf)
-            W_bnd = 1.0 / (1.0 + U_bnd)
+            W_seg = 1.0 / (1.0 + alpha_seg * U_seg)
+            W_sdf = 1.0 / (1.0 + alpha_sdf * U_sdf)
+            W_bnd = 1.0 / (1.0 + alpha_bnd * U_bnd)
 
             loss_unsup_seg = (W_seg * loss_map_seg).mean()
             loss_unsup_sdf = (W_sdf * loss_map_sdf).mean()
